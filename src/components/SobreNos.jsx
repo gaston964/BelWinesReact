6a228ff90067a3ef504a2d1bb450c76f6ecc4688
gaston964 <gastongutierrez964@gmail.com>
2023-01-09 18:21:40 -0300

    Updates y loader

--- src/components/SobreNos.jsx
+++ src/components/SobreNos.jsx
@@ -5,18 +5,25 @@ const SobreNos = () => {
     return (
         <>
             <div className="sobrenosContainer">
-                <div className="column hide">
-                    <img src='\images\bodega3.jpg' alt="Image 1" />
+                <div className="column">
+                    <img src='\images\belbodega.jpg' alt="Image 1" />
                 </div>
                 <div className="sobrenosInfo">
                     <h1>Bienvenidos a BelWines!</h1>
-                    <h3>Un poco sobre nosotros...</h3>
-                <p>Bel Wines es un emprendimiento que nació en Mayo 2020 ( pandemia ).
-                    Mi nombre es Belén Villarroel, soy Técnica en Turismo. El sector al que yo me dedico, fué
-                    muy afectado por la pandemia. Algunos fuimos suspendidos o despedidos.
-                    La idea del emprendimiento es ofrecer productos que no puedan encontrar en góndolas de supermercado</p> 
+                    <h3>Sobre nosotros...</h3>
+                    <p>Mi nombre es Belén Villarroel, soy Técnica en Turismo.</p>
+                    <p>
+                        Bel Wines es un emprendimiento que nació en Mayo 2020 ( pandemia ). El sector al que yo me dedico, fué muy afectado por la pandemia. Se presentó esta oportunidad en el sector vitivinícola, y no lo dudé ni un segundo.
+                        Comencé comprando 5 cajas y poniendo la tarjeta de crédito.
+                    </p>
+                    <p> Mis primeros clientes fueron mis amigos y conocidos. Luego de eso,el negocio comenzó a moverse, me empezaron a contactar conocidos de mis amigos y así comenzó esta aventura.
+                        Comencé a promocionar por Facebook y empecé a recibir pedidos de otras provincias.</p>
+                    <p>Detrás de este negocio hay mucho esfuerzo y dedicación. Estoy involucrada con mi emprendimiento y con la satisfacción de mis clientes.</p>
+                    <p>La idea del emprendimiento es ofrecer vinos de MENDOZA que no puedan encontrar en góndolas de supermercado, vinos de bodegas boutique o menos conocidas. </p>
+                    <h3>Hago envíos a todo el país, a través de Andesmar Cargas a domicilio o Andreani a sucursal. </h3>
+                    <h2>LOS INVITO A CONOCERME</h2>
                 </div>
-                <div className="column">
+                <div className="column hide">
                     <img src='\images\bodega2.jpg' alt="Image 2" />
                 </div>
             </div>
